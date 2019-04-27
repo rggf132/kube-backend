d471e11c389a7ba244fbe010e3ad64ec43975316
Benjamin Ramsden <benramsdensii@gmail.com>
2019-04-27 14:24:21 +0100

    Task get tasks endpoint working

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -1,7 +1,28 @@
 import express from 'express'
+import { getTasks, insertTask, deleteTask, updateTask } from "../database/actions"
 
 const router = express.Router();
 
-router.get('/', (req, res) => res.send('Tasks'))
+router.get('/', (req, res) => {
+    getTasks()
+        .then(tasks => {
+            res.send(tasks)
+        })
+        .catch(error => {
+            res.status(500).send({message:error.message})
+        })
+})
+
+router.post('/', (req,res) => {
+
+})
+
+router.put('/', (req,res) => {
+
+})
+
+router.delete('/', (req,res) => {
+
+})
 
 module.exports = router;
