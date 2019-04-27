c66d68acd179fb66f9d241415fb328b08b5c6ea6
Benjamin Ramsden <benramsdensii@gmail.com>
2019-04-27 10:04:58 +0100

    Create a tasks route

--- index.js
+++ index.js
@@ -4,4 +4,7 @@ const port = 3000
 
 app.get('/', (req, res) => res.send('Hello World!'))
 
+const tasksRoute = require('./controllers/tasks')
+app.use('/tasks', tasksRoute);
+
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
