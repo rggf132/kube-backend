b4e80398c6ee74c16ffcffd8a8757df78396aeeb
Benjamin Ramsden <benramsdensii@gmail.com>
2019-04-27 10:23:54 +0100

    Configure babel to allow preset env (import syntax, etc)

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
-const express = require('express')
+import express from 'express'
+
+//Routes
+import tasksRoute from './controllers/tasks'
+
 const app = express()
 const port = 3000
 
 app.get('/', (req, res) => res.send('Hello World!'))
 
-const tasksRoute = require('./controllers/tasks')
 app.use('/tasks', tasksRoute);
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
