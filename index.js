f7268a1ebab447af181571f4395518c894d9d269
Benjamin Ramsden <benramsdensii@gmail.com>
2019-04-27 15:55:56 +0100

    Allow cross origin requests

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 import express from 'express'
 import bodyParser from 'body-parser'
+import cors from 'cors'
 import tasksRoute from './controllers/tasks'
 
 const app = express()
 const port = 3000
 
 //Middleware
+app.use(cors())
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
