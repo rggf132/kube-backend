d2735f5051f069d29ccc41b02903b005fb11eb3a
Mariyan Tenev <rggf132@gmail.com>
2019-05-26 11:56:56 +0100

    Removed tokens and switched to session based authentication. Added function setupPassport() to help abstract most of the passport code.

--- index.js
+++ index.js
@@ -1,55 +1,45 @@
-import express from 'express'
-import bodyParser from 'body-parser'
-import cors from 'cors'
-import tasksRoute from './controllers/tasks'
+import express from "express";
+import bodyParser from "body-parser";
+import cors from "cors";
+import tasksRoute from "./controllers/tasks";
 
-
-import authRoute from './controllers/auth'
+import authRoute from "./controllers/auth";
 // import passport and passport-jwt modules
-import passport from 'passport'
-import passportJWT from 'passport-jwt'
-import { getUser } from "./database/actions";
+var passport = require("passport"),
+  LocalStrategy = require("passport-local").Strategy;
+import { setupPassport } from "./services/passport";
 
-// ExtractJwt to help extract the token
-let ExtractJwt = passportJWT.ExtractJwt;
-// JwtStrategy which is the strategy for the authentication
-let JwtStrategy = passportJWT.Strategy;
-let jwtOptions = {};
-jwtOptions.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken();
-jwtOptions.secretOrKey = 'wowwow'
-
-// lets create our strategy for web token
-let strategy = new JwtStrategy(jwtOptions, function(jwt_payload, next) {
-    console.log('payload received', jwt_payload);
-    let user = getUser({ id: jwt_payload.id });
-    if (user) {
-        next(null, user);
-    } else {
-        next(null, false);
-    }
-});
-
-// use the strategy
-passport.use(strategy);
-
-
-
-const app = express()
-const port = 3000
+const app = express();
+const port = 3000;
 
 //Middleware
-app.use(cors())
-app.use(bodyParser.urlencoded({ extended: false }))
-app.use(bodyParser.json())
-
-
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 app.use(passport.initialize());
+app.use(passport.session());
+setupPassport(passport, LocalStrategy);
+
+app.get("/success", (req, res) =>
+  res.send("Welcome " + req.query.email + "!!")
+);
+app.get("/error", (req, res) => res.send("error logging in" + req.query.email));
 
 //Endpoints
-app.get('/', (req, res) => res.send('Hello World!'))
-app.use('/tasks', tasksRoute);
-app.use('/auth', authRoute);
+app.get("/", (req, res) => res.send("Hello World!"));
+app.use("/tasks", tasksRoute);
+app.use("/auth", authRoute);
+
+app.post(
+  "/login",
+  passport.authenticate("local", { failureRedirect: "/error" }),
+  function(req, res) {
+    res.redirect("/success?email=" + req.user.email);
+  }
+);
 
 //Expose
-export default app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+export default app.listen(port, () =>
+  console.log(`Example app listening on port ${port}!`)
+);
