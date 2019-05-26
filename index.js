a4e141cefd57443224df83c00c4c6d70de8f4763
Mariyan Tenev <rggf132@gmail.com>
2019-05-26 14:08:41 +0100

    Added express-session middleware to properly implement the use of cookies for auth. Added uuid and session-file-store as well.

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 import express from "express";
 import bodyParser from "body-parser";
 import cors from "cors";
+const uuid = require("uuid/v4");
+const session = require("express-session");
+const FileStore = require("session-file-store")(session);
 import tasksRoute from "./controllers/tasks";
 
 const passport = require("passport"),
@@ -14,6 +17,20 @@ const port = 3000;
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+
+app.use(
+  session({
+    genid: req => {
+      console.log("Inside session middleware genid function");
+      console.log(`Request object sessionID from client: ${req.sessionID}`);
+      return uuid(); // use UUIDs for session IDs
+    },
+    store: new FileStore(),
+    secret: "keyboard cat",
+    resave: false,
+    saveUninitialized: true
+  })
+);
 
 app.use(passport.initialize());
 app.use(passport.session());
