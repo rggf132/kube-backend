57054c274c62a4abba0255cc2eaa4523b28f6e98
Mariyan Tenev <rggf132@gmail.com>
2019-06-10 18:11:10 +0100

    Refactored some of the imports, which used old syntax. Added multiple Cors Options. Added cookie data to session.

--- index.js
+++ index.js
@@ -1,20 +1,30 @@
 import express from "express";
 import bodyParser from "body-parser";
 import cors from "cors";
-const uuid = require("uuid/v4");
-const session = require("express-session");
+import uuid from "uuid/v4";
+import session from "express-session";
 const FileStore = require("session-file-store")(session);
-import tasksRoute from "./controllers/tasks";
+import passport from "passport";
+const LocalStrategy = require("passport-local").Strategy;
 
-const passport = require("passport"),
-  LocalStrategy = require("passport-local").Strategy;
+import tasksRoute from "./controllers/tasks";
 import { setupPassportLocal } from "./passport/passport-local-strategy";
+import { isAuthenticated } from "./passport/isAuthenticated";
 
 const app = express();
 const port = 3000;
 
 //Middleware
-app.use(cors());
+var corsOptions = {
+  origin: "http://localhost:3001",
+  credentials: true,
+  allowedHeaders: ["sessionID", "Content-Type", "Authorization"],
+  exposedHeaders: ["sessionID"],
+  methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+  preflightContinue: true
+};
+
+app.use(cors(corsOptions));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
@@ -27,8 +37,15 @@ app.use(
     },
     store: new FileStore(),
     secret: "keyboard cat",
+    cookie: {
+      path: "/",
+      httpOnly: true,
+      secure: false,
+      maxAge: 1000 * 60 * 60 * 24,
+      domain: "localhost"
+    },
     resave: false,
-    saveUninitialized: true
+    saveUninitialized: false
   })
 );
 
@@ -37,8 +54,8 @@ app.use(passport.session());
 setupPassportLocal(app, passport, LocalStrategy);
 
 //Endpoints
-app.get("/", (req, res) => res.send("Hello World!"));
-app.use("/tasks", tasksRoute);
+
+app.use("/tasks", isAuthenticated, tasksRoute);
 
 //Expose
 export default app.listen(port, () =>
