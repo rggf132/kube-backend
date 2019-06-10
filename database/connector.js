a3978f01233f63e766dad2504469497b48ebd19e
Mariyan Tenev <rggf132@gmail.com>
2019-06-11 00:25:33 +0100

    Change of styling

--- database/connector.js
+++ database/connector.js
@@ -1,33 +1,36 @@
-import {getArgument} from "./arguments";
+import { getArgument } from "./arguments";
 
-const Sequelize = require('sequelize');
+const Sequelize = require("sequelize");
 
-const dbuser = getArgument('dbuser')
-const dbpass = getArgument('dbpass')
-const dbhost = getArgument('dbhost')
+const dbuser = getArgument("dbuser");
+const dbpass = getArgument("dbpass");
+const dbhost = getArgument("dbhost");
 
-const sequelize = new Sequelize(`mysql://${dbuser}:${dbpass}@${dbhost}/taskApp`);
+const sequelize = new Sequelize(
+  `mysql://${dbuser}:${dbpass}@${dbhost}/taskApp`
+);
 
 //Task Model
-const Task = sequelize.define('task',{
-    title: Sequelize.STRING,
-    description: Sequelize.STRING,
-    status: Sequelize.STRING
-})
+const Task = sequelize.define("task", {
+  title: Sequelize.STRING,
+  description: Sequelize.STRING,
+  status: Sequelize.STRING
+});
 
 //User Model
-const User = sequelize.define('user',{
-    email: Sequelize.STRING,
-    password: Sequelize.STRING
-})
+const User = sequelize.define("user", {
+  email: Sequelize.STRING,
+  password: Sequelize.STRING
+});
 
-sequelize.sync()
-    .then(result => {
-        console.log("Database synced")
-    })
-    .catch(error => {
-        console.log(error)
-        console.log("Error syncing database")
-    })
+sequelize
+  .sync()
+  .then(result => {
+    console.log("Database synced");
+  })
+  .catch(error => {
+    console.log(error);
+    console.log("Error syncing database");
+  });
 
-export { Task, User }
+export { Task, User };
