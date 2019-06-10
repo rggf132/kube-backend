e451162e4fdc9eaa576dfd6d79ebe4915834d02c
Mariyan Tenev <rggf132@gmail.com>
2019-06-10 18:14:08 +0100

    Added isAuthenticated middleware, to deny access if user cookie and session does not match

---
+++ passport/isAuthenticated.js
@@ -0,0 +1,8 @@
+export const isAuthenticated = (req, res, next) => {
+  if (req.isAuthenticated()) {
+    //req.isAuthenticated() will return true if user is logged in
+    next();
+  } else {
+    res.redirect("/login");
+  }
+};
