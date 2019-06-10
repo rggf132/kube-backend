c85413a85cb02fa2dc045a7db69604745d063b52
Mariyan Tenev <rggf132@gmail.com>
2019-06-10 18:13:04 +0100

    Refactored styling because of prettify

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -1,60 +1,65 @@
-import express from 'express'
-import { getTasks, insertTask, deleteTask, updateTask } from "../database/actions"
+import express from "express";
+import {
+  getTasks,
+  insertTask,
+  deleteTask,
+  updateTask
+} from "../database/actions";
 
 const router = express.Router();
 
-router.get('/', (req, res) => {
-    getTasks()
-        .then(tasks => {
-            res.send(tasks)
-        })
-        .catch(error => {
-            console.log(error)
-            res.status(500).send('Error getting tasks')
-        })
-})
+router.get("/", (req, res) => {
+  getTasks()
+    .then(tasks => {
+      res.send(tasks);
+    })
+    .catch(error => {
+      console.log(error);
+      res.status(500).send("Error getting tasks");
+    });
+});
 
-router.post('/', (req,res) => {
-    const task = req.body
+router.post("/", (req, res) => {
+  const task = req.body;
 
-    insertTask(task)
-        .then(task => {
-            res.send(task)
-        })
-        .catch(error => {
-            console.log(error)
-            res.status(500).send('Error creating task')
-        })
-})
+  insertTask(task)
+    .then(task => {
+      res.send(task);
+    })
+    .catch(error => {
+      console.log(error);
+      res.status(500).send("Error creating task");
+    });
+});
 
-router.put('/:id', (req,res) => {
-    const id = req.params.id
-    const task = req.body
+router.put("/:id", (req, res) => {
+  const id = req.params.id;
+  const task = req.body;
 
-    updateTask(id,task)
-        .then(task => {
-            res.send(task)
-        })
-        .catch(error => {
-            console.log(error)
-            res.status(500).send('Error updating task')
-        })
-})
+  updateTask(id, task)
+    .then(task => {
+      res.send(task);
+    })
+    .catch(error => {
+      console.log(error);
+      res.status(500).send("Error updating task");
+    });
+});
 
-router.delete('/:id', (req,res) => {
-    const id = req.params.id
+router.delete("/:id", (req, res) => {
+  const id = req.params.id;
 
-    deleteTask(id)
-        .then(result => {
-            if(result===0) {
-                return res.status(404).send('Task does not exist')
-            }
-            res.send('Task deleted')
-        })
-        .error(error => {
-            console.log(error)
-            res.status(500).send('Error deleting task')
-        })
-})
+  deleteTask(id)
+    .then(result => {
+      if (result === 0) {
+        return res.status(404).send("Task does not exist");
+      }
+      res.send("Task deleted");
+    })
+    .error(error => {
+      console.log(error);
+      res.status(500).send("Error deleting task");
+    });
+});
 
 module.exports = router;
