a4856c9e9a039d09e94e2c0c42f500bdcb37c797
Mariyan Tenev <rggf132@gmail.com>
2019-06-11 00:25:17 +0100

    Modified the response, as this is more appropriate

--- passport/isAuthenticated.js
+++ passport/isAuthenticated.js
@@ -3,6 +3,7 @@ export const isAuthenticated = (req, res, next) => {
     //req.isAuthenticated() will return true if user is logged in
     next();
   } else {
-    res.redirect("/login");
+    console.log("Not authenticated");
+    res.status(203).send({ error: "Not Authenticated" });
   }
 };
