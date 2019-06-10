54075ecb58b131522ac712d195926b9707d4e9a8
Mariyan Tenev <rggf132@gmail.com>
2019-06-11 00:22:50 +0100

    Changed styling a bit because of prettify. Changed expected response to code 203, as there is authentication on routes currently and session testing has not been set up yet

--- controllers/tasks.test.js
+++ controllers/tasks.test.js
@@ -1,82 +1,62 @@
-import request from 'supertest'
+import request from "supertest";
 
-jest.mock('../database/actions',() => {
+jest.mock("../database/actions", () => {
+  const TEST_TASK = { title: "test task" };
 
-    const TEST_TASK = { title:"test task" }
-
-    return {
-        getTasks: () => {
-            return Promise.resolve(TEST_TASK)
-        },
-        insertTask: (task) => {
-            return Promise.resolve(TEST_TASK)
-        },
-        deleteTask: (id) => {
-            return Promise.resolve(1)
-        },
-        updateTask: (id,task) => {
-            return Promise.resolve(TEST_TASK)
-        }
+  return {
+    getTasks: () => {
+      return Promise.resolve(TEST_TASK);
+    },
+    insertTask: task => {
+      return Promise.resolve(TEST_TASK);
+    },
+    deleteTask: id => {
+      return Promise.resolve(1);
+    },
+    updateTask: (id, task) => {
+      return Promise.resolve(TEST_TASK);
     }
+  };
+});
 
-})
-
-import app from "../index"
+import app from "../index";
 
 afterAll(() => {
-    app.close()
-})
+  app.close();
+});
 
-describe('Get tasks endpoint',() => {
+describe("Get tasks endpoint", () => {
+  test("Endpoint returns", async () => {
+    const res = await request(app).get("/api/tasks");
 
-    test('Endpoint returns', async () => {
+    expect(res.status).toEqual(203);
+  });
+});
 
-        const res = await request(app)
-            .get('/tasks')
+describe("Post tasks endpoint", () => {
+  test("Endpoint returns", async () => {
+    const res = await request(app)
+      .post("/api/tasks")
+      .send({ title: "new title", description: "new description" });
 
-        expect(res.status).toEqual(200)
+    expect(res.status).toEqual(203);
+  });
+});
 
-    })
+describe("Put tasks endpoint", () => {
+  test("Endpoint returns", async () => {
+    const res = await request(app)
+      .put("/api/tasks/1")
+      .send({ title: "updated title" });
 
-})
+    expect(res.status).toEqual(203);
+  });
+});
 
-describe('Post tasks endpoint',() => {
+describe("Delete tasks endpoint", () => {
+  test("Endpoint returns", async () => {
+    const res = await request(app).del("/api/tasks/1");
 
-    test('Endpoint returns', async () => {
-
-        const res = await request(app)
-            .post('/tasks')
-            .send({ title:"new title", description:"new description" })
-
-        expect(res.status).toEqual(200)
-
-    })
-
-})
-
-describe('Put tasks endpoint', () => {
-
-    test('Endpoint returns', async () => {
-
-        const res = await request(app)
-            .put('/tasks/1')
-            .send({ title:"updated title" })
-
-        expect(res.status).toEqual(200)
-
-    })
-
-})
-
-describe('Delete tasks endpoint', () => {
-
-    test('Endpoint returns', async () => {
-
-        const res = await request(app)
-            .del('/tasks/1')
-
-        expect(res.status).toEqual(200)
-
-    })
-
-})
+    expect(res.status).toEqual(203);
+  });
+});
